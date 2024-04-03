829ba16361ee55cdafbbf44c5e8f159d5bced21f
AnshumanNeon <anshumanneon@gmail.com>
2024-04-03 21:52:57 +0530

    something

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -23,8 +23,34 @@ void lexer_tokenize(const char* filename)
     tokens_length++;
   }
 
+  char ch = fgetc(file);
+
+  int line_n = 1, col_n = 1, i = 0;
+
+  while(!feof(file)) {
+    ch = fgetc(file);
+
+    // line numbers
+    if(ch == '\n') {
+      line_n++;
+      col_n = 1;
+    }
+
+    tokens[i].line = line_n;
+
+    // col number
+    if(ch == tokens[i].token[0]) {
+      tokens[i].col = col_n;
+
+      // go to next token only after assigning both line and column
+      i++;
+    }
+
+    col_n++;
+  }
+
   for(int i = 0; i < tokens_length; i++) {
-    printf("%s\n", tokens[i].token);
+    printf("%s : line = %d, col = %d\n", tokens[i].token, tokens[i].line, tokens[i].col);
   }
 
   fclose(file);
--- src/lexer/tokens.h
+++ src/lexer/tokens.h
@@ -4,9 +4,6 @@ typedef struct
 {
   int token_type;
   char token[50];
-
-  // debug info
-  int line, col;
 } Token;
 
 // tokens
